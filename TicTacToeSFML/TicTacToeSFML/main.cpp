ed63eb4a945c56bd01d5cc21a86cfdee167d6052
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2020-02-13 20:08:22 +0200

    Deleted test code from main.cpp

--- TicTacToeSFML/TicTacToeSFML/main.cpp
+++ TicTacToeSFML/TicTacToeSFML/main.cpp
@@ -1,23 +1,6 @@
-#include <SFML/Graphics.hpp>
-
 #include <iostream>
 
-using namespace sf;
-
 int main() {
-	RenderWindow window(VideoMode(1024, 1024), "TicTacToeSFML");
-
-	while(window.isOpen()) {
-		Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == Event::Closed) {
-				window.close();
-			}
-		}
-		window.clear();
-
-		window.display();
-	}
-
-	return 0;
+	
+	return EXIT_SUCCESS;
 }
